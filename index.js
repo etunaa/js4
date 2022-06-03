5626cfddac009552b1bb75a8153b745523fb9722
etunaa <104025100+etunaa@users.noreply.github.com>
2022-06-03 17:54:24 +0400

    13

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
-function Car(name, model, year){
-    this.name=name;
-    this.model=model;
-    this.year=year;
-    this.getCarInfo=function(){
-        document.write(this.name+ "  " + "model"+ "  " +this.model+"  "+ "relesed in"+"  "+this.year)
-    }
-}
+// function Car(name, model, year){
+//     this.name=name;
+//     this.model=model;
+//     this.year=year;
+//     this.displayInfo=function(){
+//         document.write(this.name+ "  " + "model"+ "  " +this.model+"  "+ "relesed in"+"  "+this.year);
+//     };
+// };
 
-var tesla=new Car("Tesla","S",2015)
+// var tesla=new Car("Tesla","S",2015);
+// tesla.displayInfo();
+
+// function Car(name, model, year){
+//     this.name=name;
+//     this.model=model;
+//     this.year=year;
+//     this.getCarInfo=function(){
+//         document.write(this.name+ "  " + "model"+ "  " +this.model+"  "+ "relesed in"+"  "+this.year);
+//     };
+// };
+
+// function Person(name, age, cars){
+//     this.name=name;
+//     this.age=age;
+//     this.cars=cars;
+//     this.driveCar=function(car){
+//         document.write("  "+this.name+ "  " + "has a car"+"  " +car.name+"  "+"model"+"  "+car.model+"  "+"released in"+"  "+car.year)
+//     };
+//     this.displayInfo=function(){
+//         document.write("  "+this.age +"  "+"years old")
+//     };
+
+// }
+
+// var tom=new Person("Tom",26);
+// tom.displayInfo();
+
+// var tesla=new Car("Tesla","S",2015);
+// tom.driveCar(tesla);
+
